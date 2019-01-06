d1d06a394fd67bee69825a4683bc69e9c77c29cb
9min <gm0711@nate.com>
2019-01-06 12:15:12 +0900

    #1.7 API & Schema Structue part Two

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Options } from 'graphql-yoga';
 import app from './app';
 
-const PORT:number|string = process.env.PORT || 4000;
-const PLAYGROUND_ENDPOINT:string = '/playground';
-const GRAPHQL_ENDPOINT:string = '/graphql';
+const PORT: number|string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT: string = '/playground';
+const GRAPHQL_ENDPOINT: string = '/graphql';
 
-const appOptions:Options = {
+const appOptions: Options = {
   port: PORT,
   playground: PLAYGROUND_ENDPOINT,
   endpoint: GRAPHQL_ENDPOINT,
