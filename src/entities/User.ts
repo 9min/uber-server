26a7087e4bf71c12752aab5b46f18814bc5b6236
9min <gm0711@nate.com>
2019-01-08 23:04:59 +0900

    1.14 User Entity part Two

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -24,9 +24,6 @@ class User extends BaseEntity {
   @Column({ type: 'text' })
   lastName: String;
 
-  @Column({ type: 'text' })
-  fullName: String;
-
   @Column({ type: 'int' })
   age: number;
 
@@ -41,6 +38,28 @@ class User extends BaseEntity {
 
   @Column({ type: 'text' })
   profilePhoto: string;
+
+  @Column({ type: 'boolean', default: false })
+  isDriving: boolean;
+
+  @Column({ type: 'boolean', default: false })
+  isRiding: boolean;
+
+  @Column({ type: 'boolean', default: false })
+  isTaken: boolean;
+
+  @Column({ type: 'double precision', default: 0 })
+  lastLng: number;
+
+  @Column({ type: 'double precision', default: 0 })
+  lastLat: number;
+
+  @Column({ type: 'double precision', default: 0 })
+  lastOrientation: number;
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
 
   @CreateDateColumn() createAt: string;
   @CreateDateColumn() updateAt: string;
